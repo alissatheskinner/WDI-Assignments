881cd1a784d8517518a1da1a3ed8e37a88fbe669
Alissa Skinner <skinner_alissa@gmail.com>
2017-01-22 22:16:31 -0700

    final javascript capstone

--- Horoscope_Challenge/script.js
+++ Horoscope_Challenge/script.js
@@ -1,20 +1,68 @@
 $(document).ready(function(){
 
 
+	function celebMatch(sign) {
+	  $.ajax({
+          url: 'https://node-horoscopes.herokuapp.com/api/horoscopes/celebrities/' + sign,
+          success: function(response) {
+          	
+          	$('#celebrityName').html(response.actors[0].name);
+          	$('#celebImage').attr("src", response.actors[0].imageUrl);
+          }
+         
+	   })
+	}
+
+	function dailyHoroscope(sign) {
+		$.ajax({
+			url: 'https://node-horoscopes.herokuapp.com/api/horoscopes/today/' + sign,
+			success: function(response) {
+				
+				$('#yourSign').text(response.sign);
+				$('#guts p').text(response.horoscope);
+				
+			}
+			
+        })
+	}
+
+
+	function Match(fromSign, toSign){
+
+		$.ajax({
+			url: 'https://node-horoscopes.herokuapp.com/api/horoscopes/dating/' + fromSign + "/" + toSign,
+			success: function(response) {
+				
+				// $('#sign1, #sign2').change(response.text);
+				$('#yourMatch').text(response.text);
+				
+			}
+		})
+	}
+
+
+//Event Handlers 
+	
+	$('#celebButton').click(function(event){
+		var selectedSign = $("#celebSign").find(':selected').val();
+		celebMatch(selectedSign);
+		dailyHoroscope(selectedSign);
+	});
+
+
+	$('#matchButton').click(function(event){
+		var fromMatch = $("#sign1").find(':selected').val();
+		var toMatch = $("#sign2").find(':selected').val();
+		Match(fromMatch, toMatch);
+	})
+
+
+});
 
 
 
 
-$('#celebImage').attr("src", );
 
 
-})
 
 
-// $('a #wikiLink').click(function(event)) {
-// 	return (url: "https://en.wikipedia.org/wiki/Astrological_sign");
-// }
-
-
-// $("#celebrityName").html(response.Title);   
-// $("#image").attr("src", response.Poster); 
